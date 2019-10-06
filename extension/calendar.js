a9a506eda75e54df59f88ca2d6b6b775393f5e06
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-10-06 23:20:49 +0200

    Getting all teacher ids

--- extension/calendar.js
+++ extension/calendar.js
@@ -1,1 +1,2 @@
-document.body.style.border = "5px solid blue";
+document.body.style.border = "5px solid yellow";
+document.getElementsByClassName("list-group teachers")[0].getElementsByTagName("label"));

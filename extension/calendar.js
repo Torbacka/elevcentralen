cf7581184611fe9e848fb93ff760515f471c0d4c
Daniel Törbacka <daniel.torbacka@gmail.com>
2019-10-08 07:49:46 +0200

    Inserted html with firefox extension

--- extension/calendar.js
+++ extension/calendar.js
@@ -1,6 +1,11 @@
 document.body.style.border = "5px solid yellow";
 const teachers = [...document.getElementsByClassName("list-group teachers")[0].getElementsByTagName("label")];
 
+var newItem = document.createElement("LI");       // Create a <li> node
+var textnode = document.createTextNode("Water");  // Create a text node
+newItem.appendChild(textnode);
+document.getElementsByClassName("tab-content transparent")[0].insertBefore(newItem, document.getElementsByClassName("tab-content transparent")[0].childNodes[0]);
+
 
 teachers.forEach(label =>{
     console.log(label.getAttribute("data-name"));
